1cf1a5bc5a65b405ad8f59bb11faa2d7b5c8798d
iti tripathy <iti.tripathy@gmail.com>
2022-08-23 17:29:53 +1000

    Reading the JSON file

--- src/wordle/Main.java
+++ src/wordle/Main.java
@@ -1,9 +1,52 @@
 package wordle;
 
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		JSONParser parser = new JSONParser();
+		try {
+			Object obj = parser.parse(new FileReader("C:\\nology\\java-wordle\\src\\wordle\\word-list.json"));
+			JSONObject jsonObject = (JSONObject)obj;
+			JSONArray jsonArray = (JSONArray) jsonObject.get("words");
+			
+			//Iterator iterator = jsonArray.iterator();
+	        
+			/*
+			 * while (iterator.hasNext()) { System.out.println(iterator.next()); }
+			 */
+			
+			Random r = new Random();
+			int num1 = r.nextInt(jsonArray.size());
+			String randomWord = jsonArray.get(num1).toString();
+			System.out.println(randomWord);
+			
+	         
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
