b5ac67a19612e1b442b6ce21dbb0707a79e5d21d
iti tripathy <iti.tripathy@gmail.com>
2022-08-23 19:20:34 +1000

    Input Validation

---
+++ src/wordle/GetARandomWordClass.java
@@ -0,0 +1,55 @@
+package wordle;
+
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class GetARandomWordClass {
+	
+	public static String GetARandomWord() {
+		
+		JSONParser parser = new JSONParser();
+	    String randomWord = null;
+		
+		try {
+			Object obj = parser.parse(new FileReader("C:\\nology\\java-wordle\\src\\wordle\\word-list.json"));
+			JSONObject jsonObject = (JSONObject)obj;
+			JSONArray jsonArray = (JSONArray) jsonObject.get("words");
+			
+			//Iterator iterator = jsonArray.iterator();
+	        
+			/*
+			 * while (iterator.hasNext()) { System.out.println(iterator.next()); }
+			 */
+			
+			Random r = new Random();
+			int num1 = r.nextInt(jsonArray.size());
+			randomWord = jsonArray.get(num1).toString();
+			//System.out.println(randomWord);
+			
+			//return randomWord;
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return randomWord;
+	}
+
+}
--- src/wordle/Main.java
+++ src/wordle/Main.java
@@ -15,39 +15,45 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Main {
+	
+	public static Scanner s = new Scanner(System.in);
+	
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	
 
 	public static void main(String[] args) {
 		
-		JSONParser parser = new JSONParser();
-		try {
-			Object obj = parser.parse(new FileReader("C:\\nology\\java-wordle\\src\\wordle\\word-list.json"));
-			JSONObject jsonObject = (JSONObject)obj;
-			JSONArray jsonArray = (JSONArray) jsonObject.get("words");
+		int numberOfTries = 0;
+		System.out.println("Please Enter A Five-Lettered Word");
+		
+		String wordOfTheDay = GetARandomWordClass.GetARandomWord();
+		System.out.println(wordOfTheDay);
+		
+		char[] answer = new char[5];
+		char[] input = new char[5];
+		boolean done = false;
+		
+		while(!done) {
 			
-			//Iterator iterator = jsonArray.iterator();
-	        
-			/*
-			 * while (iterator.hasNext()) { System.out.println(iterator.next()); }
-			 */
-			
-			Random r = new Random();
-			int num1 = r.nextInt(jsonArray.size());
-			String randomWord = jsonArray.get(num1).toString();
-			System.out.println(randomWord);
-			
-	         
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+			numberOfTries++;
+			String inputString = s.nextLine().toLowerCase();
+			if(inputString.length() > 5 || inputString.length() < 5 || inputString.length() == 0) {
+				System.out.println("Invalid Input !! Please Enter Only A Five-Lettered Word!!");
+			}
+			while(inputString.length() < 5) {
+				inputString = s.nextLine().toLowerCase();
+			}
+			for(int i=0; i<5; i++) {
+				answer[i] = wordOfTheDay.charAt(i);
+				
+				input[i] = inputString.charAt(i);
+				
+			}
+			done = true;
+			System.out.println(answer);
+			System.out.println(input);
+		}		
 	}
-
 }
